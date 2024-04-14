fe8142c38e33ce41b5c07826b8daf86ad0e5b21f
dsagi727 <dsagi727@gmail.com>
2024-04-14 11:53:47 +0200

    add dev/prod more complex error management

--- api/src/controllers/error/error.controller.js
+++ api/src/controllers/error/error.controller.js
@@ -1,11 +1,36 @@
-const logger = require("../../config/logger");
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    //!Operational, trusted error: send message to client
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    //! Programing or unknown error: DO NOT leak error details to the client
+    console.error("ERROR", err);
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+};
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
-  logger.error(`ERROR ${err.statusCode}: ${err.message}`);
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if ((process.env.NODE_ENV = "development")) {
+    sendErrorDev(err, res);
+  } else if ((process.env.NODE_ENV = "production")) {
+    sendErrorProd(err, res);
+  }
 };
