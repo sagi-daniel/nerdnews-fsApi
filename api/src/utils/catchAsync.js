dd706548954ee54a76d945db72a1141cc95ecf5a
dsagi727 <dsagi727@gmail.com>
2024-04-14 11:49:13 +0200

    add a wrapper to the project

--- api/src/utils/catchAsync.js
+++ api/src/utils/catchAsync.js
@@ -1,11 +1,5 @@
-const logger = require("../config/logger");
-
 module.exports = catchAsync = (fn) => {
   return (req, res, next) => {
-    fn(req, res, next).catch((err) => {
-      logger.error(err.message);
-
-      next(err);
-    });
+    fn(req, res, next).catch((err) => next(err));
   };
 };
