938f0166177e5f440ce767322cedc3d00e1bfd72
dsagi727 <dsagi727@gmail.com>
2024-05-03 23:09:15 +0200

    add security libs

--- api/src/app.js
+++ api/src/app.js
@@ -1,18 +1,56 @@
 const express = require("express");
 const morgan = require("morgan");
 const cron = require("node-cron");
+//Security libs
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+
 const app = express();
 const AppError = require("./utils/appError");
 
+// *GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
+//Limit request from same API
+app.use(
+  rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many request from this IP, please try again later!",
+  })
+);
+
+//Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+//Data sanitization against noSWL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+app.use(xss());
+
+//Prevent paramter pollution
+app.use(
+  hpp({
+    whitelist: [],
+  })
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
-//* ROUTES
+//* ROUTES MIDDLEWARES
 app.use("/user", require("./controllers/user/user.routes"));
+
 app.use(
   "/upcomingmovie",
   require("./controllers/upcomingmovie/upcomingMovie.routes")
@@ -24,7 +62,7 @@ app.use(
   require("./controllers/rss/rssCategory/rssCategory.routes")
 );
 
-// //*SCHEDULED TASKS
+//*SCHEDULED TASKS
 cron.schedule(
   "* * * * *",
   require("./controllers/scheduler/scheduler.controller")
@@ -34,6 +72,7 @@ app.all("*", (req, res, next) => {
   next(new AppError(`Could not found ${req.originalUrl} on this server!`, 404));
 });
 
+//*GLOBAL MIDDLEWARE
 app.use(require("./controllers/error/error.controller"));
 
 module.exports = app;
