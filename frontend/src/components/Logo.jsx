eaf90931fa7ed0375e9ec11c48996fa54c18891c
dsagi727 <dsagi727@gmail.com>
2024-05-27 00:01:31 +0200

    logo, toggle, appplayout init

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,6 +1,6 @@
 function Logo() {
   return (
-    <div className="w-96">
+    <div className="h-auto w-56 text-left">
       <img src="./assets/logo/logo-darkbg.svg" alt="geekHUB logo" />
     </div>
   );
