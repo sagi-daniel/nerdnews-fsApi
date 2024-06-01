0b57da0b77952878a1b9cc1e763901785cf2966f
dsagi727 <dsagi727@gmail.com>
2024-06-01 18:46:47 +0200

    change root style.css and add contants

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 // import { Toaster } from 'react-hot-toast';
 
+//CONTEXTS
 import { DarkModeProvider } from './context/DarkModeContext';
 
+//COMPONENTS
 import AppLayout from './components/parts/AppLayout';
 import Home from './pages/Home';
 import News from './pages/News';
@@ -16,35 +19,41 @@ import Login from './pages/Login';
 
 import PageNotFound from './pages/PageNotFound';
 
+//CONSTANTS
+import { QUERY_DEFAULT_OPTIONS } from './utils/constants';
+
 function App() {
   return (
     <>
-      <DarkModeProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route
-              element={
-                // <ProtectedRoute>
-                <AppLayout />
-                // </ProtectedRoute>
-              }
-            >
-              <Route index element={<Navigate replace to="Home" />} />
-              <Route path="home" element={<Home />} />
-              <Route path="news" element={<News />} />
-              <Route path="movies" element={<UpcomingMovies />} />
-              <Route path="cidr" element={<IpCidrCalculator />} />
-              <Route path="signup" element={<SignUp />} />
-              <Route path="login" element={<Login />} />
-              {/* <Route path="bookings/:bookingId" element={<Booking />} />
+      <QueryClientProvider client={new QueryClient(QUERY_DEFAULT_OPTIONS)}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <DarkModeProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route
+                element={
+                  // <ProtectedRoute>
+                  <AppLayout />
+                  // </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="Home" />} />
+                <Route path="home" element={<Home />} />
+                <Route path="news" element={<News />} />
+                <Route path="movies" element={<UpcomingMovies />} />
+                <Route path="cidr" element={<IpCidrCalculator />} />
+                <Route path="signup" element={<SignUp />} />
+                <Route path="login" element={<Login />} />
+                {/* <Route path="bookings/:bookingId" element={<Booking />} />
             <Route path="checkin/:bookingId" element={<Checkin />} />
             <Route path="account" element={<Account />} /> */}
-            </Route>
+              </Route>
 
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </DarkModeProvider>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </DarkModeProvider>
+      </QueryClientProvider>
     </>
   );
 }
