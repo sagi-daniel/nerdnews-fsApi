a1dc828eaf3036f3b3a1153c6e90e8b1f29f762f
dsagi727 <dsagi727@gmail.com>
2024-06-01 18:49:31 +0200

    added loadingSpinner comp

---
+++ frontend/src/components/LoadingSpinner.jsx
@@ -0,0 +1,16 @@
+// src/components/LoadingSpinner.js
+import React from 'react';
+
+const LoadingSpinner = () => {
+  return (
+    <div className="flex items-center justify-center overflow-hidden">
+      <div className="relative w-16 h-16 animate-spin">
+        <div className="absolute border-4 border-t-4 border-transparent border-t-bg-light dark:border-t-primary rounded-full w-full h-full"></div>
+        <div className="absolute border-4 border-t-4 border-transparent  rounded-full w-12 h-12 top-2 left-2"></div>
+        <div className="absolute border-4 border-t-4 border-transparent  rounded-full w-8 h-8 top-4 left-4"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
