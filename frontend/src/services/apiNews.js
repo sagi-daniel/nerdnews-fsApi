bb1f1bb979e7510a3c11b57e0d4e2b30acb24bce
dsagi727 <dsagi727@gmail.com>
2024-06-02 09:01:57 +0200

    added new service

--- frontend/src/services/apiNews.js
+++ frontend/src/services/apiNews.js
@@ -1,10 +1,11 @@
 import { BASE_URL } from '../utils/constants';
 
 const getNews = async () => {
-  const response = await fetch(`${BASE_URL}/rrsnews`);
+  const response = await fetch(`${BASE_URL}/rssnews`);
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
+
   return response.json();
 };
 
