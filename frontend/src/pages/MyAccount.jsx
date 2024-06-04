352b20cbc31fbc6d695b5034a7e3aba4b517f9df
dsagi727 <dsagi727@gmail.com>
2024-06-04 22:03:21 +0200

    add content

--- frontend/src/pages/MyAccount.jsx
+++ frontend/src/pages/MyAccount.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Section from '../components/Section';
+import { formatDateIsoToNormal } from '../utils/helpers';
 
 function MyAccount() {
   const [isEditing, setIsEditing] = useState(false);
@@ -28,7 +29,26 @@ function MyAccount() {
   //     console.log('Fiók törölve');
   //   };
 
-  return <Section type="horizontal"></Section>;
+  return (
+    <Section type="horizontal" space="large">
+      <div className="flex flex-col md:w-1/2 p-10 bg-border-dark  rounded-md border border-red-800">
+        <div>
+          <h1 className="text-4xl font-bold text-center md:text-left ">Hello {user.name}!</h1>
+          <p className=" pb-6 text-center md:text-left ">content</p>
+          <p>Felhasználó létrehozva: {formatDateIsoToNormal(user.registrationDate)}</p>
+        </div>
+
+        <div className="text-center md:text-left">
+          <h2>action</h2>
+        </div>
+      </div>
+      <div className="flex flex-col p-2 md:w-1/2 border border-red-800 ">
+        <div>
+          <p>valami</p>
+        </div>
+      </div>
+    </Section>
+  );
 }
 
 export default MyAccount;
