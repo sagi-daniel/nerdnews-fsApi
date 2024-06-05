6b2a949d10a70014237929c6e2cd510349109ed3
dsagi727 <dsagi727@gmail.com>
2024-06-05 13:35:20 +0200

    added separate service to scheduler

---
+++ api/src/controllers/scheduler/rssScheduleHandling/rssHandlerService.js
@@ -0,0 +1,17 @@
+const News = require('../../../models/News.model');
+const RssSource = require('../../../models/RssSource.model');
+
+exports.create = (news) => {
+  const newNews = new News(news);
+  return newNews.save();
+};
+
+exports.findByMonthRange = (monthRange) =>
+  News.find({
+    release: {
+      $gte: new Date(new Date().getMonth() - monthRange).toISOString(),
+      $lte: new Date().toISOString(),
+    },
+  });
+
+exports.findAll = () => RssSource.find();
