29cedcb9fea193ead9037559f881b855bdf43b7d
dsagi727 <dsagi727@gmail.com>
2024-06-05 18:32:11 +0200

    added new sections

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,37 +6,14 @@ import NewsSlider from '../features/News/NewsSlider';
 function Home() {
   return (
     <>
-      <Section type="horizontal" space="large">
-        <div className="flex flex-col md:w-2/3 p-2 border border-red-800">
-          <div>
-            <h1 className="text-4xl font-bold text-center md:text-left ">Üdvözlünk a weboldalon!</h1>
-            <p className=" pb-6 text-center md:text-left ">
-              Itt naprakész infókat találsz a legújabb tech hírekről és a közelgő mozifilmekről.
-              <br /> Ne maradj le semmiről, regisztrálj!
-            </p>
-          </div>
-
-          <div className="text-center md:text-left">
-            <Link to="/singup" className="btn-primary-md">
-              Regisztráció
-            </Link>
-          </div>
-        </div>
-        <div className="flex flex-col p-2 md:w-1/3 border border-red-800 ">
-          <div>
-            <p>valami</p>
-          </div>
-        </div>
+      <Section type="vertical">
+        <h2>Legújabb Hírek</h2>
+        <NewsSlider />
       </Section>
 
       <Section type="vertical">
         <h2>Közelgő Mozifilmek</h2>
-
         <MovieSlider />
-      </Section>
-      <Section type="vertical">
-        <h2>Legújabb Hírek</h2>
-        <NewsSlider />
       </Section>
     </>
   );
