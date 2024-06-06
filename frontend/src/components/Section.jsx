6d028d934ecfac4d1290e6f2a1aa964762c211d6
dsagi727 <dsagi727@gmail.com>
2024-06-06 11:04:11 +0200

    added new style param gap

--- frontend/src/components/Section.jsx
+++ frontend/src/components/Section.jsx
@@ -1,4 +1,4 @@
-function Section({ children, type = 'horizontal', space = 'small' }) {
+function Section({ children, type = 'horizontal', space = 'small', gap = 'small' }) {
   const sizeAlignment = {
     vertical: 'flex-col',
     horizontal: 'flex-col md:flex-row',
@@ -10,7 +10,18 @@ function Section({ children, type = 'horizontal', space = 'small' }) {
     large: 'py-20 px-5 md:py-40 md:px-20',
   };
 
-  return <section className={`flex ${sizeAlignment[type]} gap-5  ${sizeSpace[space]}  w-full`}>{children}</section>;
+  const sizeGap = {
+    none: 'gap-0',
+    small: 'gap-1',
+    medium: 'gap-2',
+    large: 'gap-5',
+  };
+
+  return (
+    <section className={`flex ${sizeAlignment[type]} ${sizeGap[gap]}  ${sizeSpace[space]} h-full w-full`}>
+      {children}
+    </section>
+  );
 }
 
 export default Section;
