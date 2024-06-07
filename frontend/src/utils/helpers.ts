373e7b14c1b2ad4ddae4e2d2d3e2ad2f73a12d68
dsagi727 <dsagi727@gmail.com>
2024-06-07 11:24:26 +0200

    removed unused function

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -1,18 +1,10 @@
 import { formatDistance, parseISO } from 'date-fns';
 import { differenceInDays } from 'date-fns';
-import RatingColors from '../models/RaingColors.model';
 
 type DateInput = string | Date;
 
 interface GetTodayOptions {
   end?: boolean;
-}
-
-interface RatingColorOptions {
-  default: string;
-  under5: string;
-  under8: string;
-  under10: string;
 }
 
 export const subtractDates = (dateStr1: DateInput, dateStr2: DateInput): number =>
@@ -55,35 +47,4 @@ export const truncateText = (text: string, trunc: number): string => {
     return text;
   }
   return text.slice(0, trunc) + '...';
-};
-
-export const getRatingColors = (rating: string | number, ratingColorOptions: RatingColorOptions): RatingColors => {
-  const parsedRating = parseFloat(rating as string);
-
-  if (parsedRating === 0 || rating === undefined) {
-    return {
-      bgColor: ratingColorOptions.default,
-      textColor: ratingColorOptions.default,
-    };
-  } else if (parsedRating < 5) {
-    return {
-      bgColor: ratingColorOptions.under5,
-      textColor: ratingColorOptions.under5,
-    };
-  } else if (parsedRating < 8) {
-    return {
-      bgColor: ratingColorOptions.under8,
-      textColor: ratingColorOptions.under8,
-    };
-  } else if (parsedRating <= 10) {
-    return {
-      bgColor: ratingColorOptions.under10,
-      textColor: ratingColorOptions.under10,
-    };
-  } else {
-    return {
-      bgColor: ratingColorOptions.default,
-      textColor: ratingColorOptions.default,
-    };
-  }
 };
