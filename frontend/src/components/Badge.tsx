c63136c340248e929bc00f0d2f016268dedec93a
dsagi727 <dsagi727@gmail.com>
2024-06-07 17:04:24 +0200

    added more type of badge

--- frontend/src/components/Badge.tsx
+++ frontend/src/components/Badge.tsx
@@ -1,10 +1,13 @@
-type CategoryName = 'DEFAULT' | 'TECH' | 'CYBERSEC' | 'GAMING';
+import { CategoryName } from '../models/CategoryModel';
+
+type CategoryType = 'NORMAL' | 'EXTRA';
 
 interface BadgeProps {
   categoryName?: CategoryName;
+  type?: CategoryType;
 }
 
-function Badge({ categoryName = 'DEFAULT' }: BadgeProps) {
+function Badge({ categoryName = 'DEFAULT', type = 'EXTRA' }: BadgeProps) {
   const categoriesColorOptions: Record<CategoryName, string> = {
     DEFAULT: 'bg-secondary text-secondary-content',
     TECH: 'bg-tech text-tech-content',
@@ -12,10 +15,15 @@ function Badge({ categoryName = 'DEFAULT' }: BadgeProps) {
     GAMING: 'bg-gaming text-gaming-content',
   };
 
+  if (type === 'EXTRA')
+    return (
+      <span className={` ${categoriesColorOptions[categoryName]} px-4 py-2 text-lg font-semibold rounded`}>
+        {categoryName}
+      </span>
+    );
+
   return (
-    <span className={` ${categoriesColorOptions[categoryName]} px-4 py-2 text-lg font-semibold rounded`}>
-      {categoryName}
-    </span>
+    <span className={` ${categoriesColorOptions[categoryName]} px-1 py-0.5 font-semibold rounded`}>{categoryName}</span>
   );
 }
 
