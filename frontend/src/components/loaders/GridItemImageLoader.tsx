d1b78faacb15f7a901a0000809a87e0cbf17cbf0
dsagi727 <dsagi727@gmail.com>
2024-06-08 20:02:40 +0200

    changed style

--- frontend/src/components/loaders/GridItemImageLoader.tsx
+++ frontend/src/components/loaders/GridItemImageLoader.tsx
@@ -15,7 +15,7 @@ function GridItemImageLoader({ src }: GridItemImageLoader) {
       {!loaded && <LoadingSpinner />}
       {loaded && (
         <div
-          className={`hover:opacity-40 justify-end h-full px-2 py-5 rounded-md bg-cover bg-center ease-in-out hover:scale-110 z-[-999] transition-opacity duration-500 ${
+          className={`hover:opacity-40 justify-end h-full px-2 py-5 rounded-md bg-cover bg-center ease-in-out hover:scale-110 transition-opacity duration-500 ${
             loaded ? 'opacity-30' : 'opacity-0'
           }`}
           style={{ backgroundImage: `url(${src})` }}
