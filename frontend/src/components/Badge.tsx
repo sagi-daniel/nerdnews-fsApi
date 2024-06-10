871effc82e598202e7a145b39630d3fe0ed663cf
dsagi727 <dsagi727@gmail.com>
2024-06-10 10:55:36 +0200

    extend component to handle click events

--- frontend/src/components/Badge.tsx
+++ frontend/src/components/Badge.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent } from 'react';
 import { CategoryName } from '../models/CategoryModel';
 
 type CategoryType = 'NORMAL' | 'EXTRA';
@@ -5,9 +6,11 @@ type CategoryType = 'NORMAL' | 'EXTRA';
 interface BadgeProps {
   categoryName?: CategoryName;
   type?: CategoryType;
+  classes?: string;
+  onClick?: (event: MouseEvent<HTMLSpanElement>) => void;
 }
 
-function Badge({ categoryName = 'DEFAULT', type = 'EXTRA' }: BadgeProps) {
+function Badge({ categoryName = 'DEFAULT', type = 'EXTRA', classes, onClick }: BadgeProps) {
   const categoriesColorOptions: Record<CategoryName, string> = {
     DEFAULT: 'bg-secondary text-secondary-content',
     TECH: 'bg-tech text-tech-content',
@@ -15,15 +18,29 @@ function Badge({ categoryName = 'DEFAULT', type = 'EXTRA' }: BadgeProps) {
     GAMING: 'bg-gaming text-gaming-content',
   };
 
+  const handleClick = (event: MouseEvent<HTMLSpanElement>) => {
+    if (onClick) {
+      onClick(event);
+    }
+  };
+
   if (type === 'EXTRA')
     return (
-      <span className={` ${categoriesColorOptions[categoryName]} px-4 py-2 text-lg font-semibold rounded`}>
+      <span
+        className={`${classes} ${categoriesColorOptions[categoryName]} px-4 py-2 text-lg font-semibold rounded`}
+        onClick={handleClick}
+      >
         {categoryName}
       </span>
     );
 
   return (
-    <span className={` ${categoriesColorOptions[categoryName]} px-1 py-0.5 font-semibold rounded`}>{categoryName}</span>
+    <span
+      className={`${classes} ${categoriesColorOptions[categoryName]} px-1 py-0.5 font-semibold rounded`}
+      onClick={handleClick}
+    >
+      {categoryName}
+    </span>
   );
 }
 
