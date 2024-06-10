b0d7fb1e03e3e8e726fcb7c5f45eab61c490bac0
dsagi727 <dsagi727@gmail.com>
2024-06-10 11:07:39 +0200

    removed h-screen class

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,9 +1,7 @@
 import { ReactNode } from 'react';
 
 function Layout({ children }: { children: ReactNode }) {
-  return (
-    <div className=" flex flex-col justify-between h-screen bg-bg-light dark:bg-bg-dark px-1 md:px-5 ">{children}</div>
-  );
+  return <div className=" flex flex-col justify-between bg-bg-light dark:bg-bg-dark px-1 md:px-5 ">{children}</div>;
 }
 
 export default Layout;
