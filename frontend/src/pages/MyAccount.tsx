d76698d1e564b9e1cd0416017c1f302f7a0e1213
dsagi727 <dsagi727@gmail.com>
2024-06-10 11:11:47 +0200

    commented out some testing data and func

--- frontend/src/pages/MyAccount.tsx
+++ frontend/src/pages/MyAccount.tsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import Section from "../components/Section";
-import { formatDateIsoToNormal } from "../utils/helpers";
+import { useState } from 'react';
+import Section from '../components/Section';
+import { formatDateIsoToNormal } from '../utils/helpers';
 
 function MyAccount() {
-  const [isEditing, setIsEditing] = useState(false);
+  // const [isEditing, setIsEditing] = useState(false);
   const [user, setUser] = useState({
-    name: "John Doe",
-    username: "johndoe",
-    email: "johndoe@example.com",
-    password: "",
-    registrationDate: "2023-01-01",
+    name: 'John Doe',
+    username: 'johndoe',
+    email: 'johndoe@example.com',
+    password: '',
+    registrationDate: '2023-01-01',
   });
 
   //   const handleChange = (e) => {
@@ -33,14 +33,9 @@ function MyAccount() {
     <Section type="horizontal" space="large">
       <div className="flex flex-col md:w-1/2 p-10 bg-border-dark  rounded-md border border-red-800">
         <div>
-          <h1 className="text-4xl font-bold text-center md:text-left ">
-            Hello {user.name}!
-          </h1>
+          <h1 className="text-4xl font-bold text-center md:text-left ">Hello {user.name}!</h1>
           <p className=" pb-6 text-center md:text-left ">content</p>
-          <p>
-            Felhasználó létrehozva:{" "}
-            {formatDateIsoToNormal(user.registrationDate)}
-          </p>
+          <p>Felhasználó létrehozva: {formatDateIsoToNormal(user.registrationDate)}</p>
         </div>
 
         <div className="text-center md:text-left">
