0538e74c2e72432e19186cf7f422058b2560d21f
dsagi727 <dsagi727@gmail.com>
2024-06-10 11:09:45 +0200

    added queries to getNewsByCategory api call and refactored

--- frontend/src/services/apiNews.ts
+++ frontend/src/services/apiNews.ts
@@ -1,5 +1,4 @@
 import { BASE_URL } from '../utils/constants';
-
 import NewsModel from '../models/News.model';
 
 export async function getNews(): Promise<NewsModel[]> {
@@ -8,6 +7,19 @@ export async function getNews(): Promise<NewsModel[]> {
     throw new Error('Network response was not ok');
   }
 
+  const responseData = await response.json();
+  return responseData.data.news;
+}
+
+export async function getNewsByCategory(category: string | null, sortOrder?: string | null): Promise<NewsModel[]> {
+  const params = new URLSearchParams();
+  if (category) params.append('category', category);
+  if (sortOrder) params.append('sortOrder', sortOrder);
+
+  const response = await fetch(`${BASE_URL}/news?${params.toString()}`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
   const responseData = await response.json();
   return responseData.data.news;
 }
