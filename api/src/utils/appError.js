7df41ad1cfca8c4dd13e9255fcdb94e6e07b3e74
dsagi727 <dsagi727@gmail.com>
2024-06-11 15:26:31 +0200

    added logger

--- api/src/utils/appError.js
+++ api/src/utils/appError.js
@@ -1,10 +1,15 @@
+const logger = require('./logger');
+
 class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
 
     this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
+
+    // Log the error
+    logger.error(`Status: ${this.status}, Message: ${this.message}`);
 
     Error.captureStackTrace(this, this.constructor);
   }
