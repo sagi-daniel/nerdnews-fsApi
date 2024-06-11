d18f670d16cec1e8dfdff72cd26afffe8438dad3
dsagi727 <dsagi727@gmail.com>
2024-06-11 15:27:34 +0200

    refactored

--- api/src/utils/catchAsync.js
+++ api/src/utils/catchAsync.js
@@ -1,7 +1,5 @@
-const catchAsync = (fn) => {
-  return (req, res, next) => {
-    fn(req, res, next).catch((err) => next(err));
-  };
+const catchAsync = (fn) => (req, res, next) => {
+  fn(req, res, next).catch((err) => next(err));
 };
 
 module.exports = catchAsync;
