0c40a3f77f1a655017e41d087294c3a47716899e
dsagi727 <dsagi727@gmail.com>
2024-06-11 15:27:46 +0200

    fixed parseDate

--- api/src/utils/helpers.js
+++ api/src/utils/helpers.js
@@ -7,31 +7,37 @@ const DEFAULTS = {
 };
 
 // Function to handle date parsing with defaults
-const parseDate = (dateString) => {
+const parseDate = (dateString, dateType) => {
   const currentDate = new Date();
-  return dateString
-    ? new Date(dateString)
-    : new Date(currentDate.setMonth(currentDate.getMonth() - DEFAULTS.DATE_RANGE));
+  if (dateString) {
+    return new Date(dateString);
+  } else {
+    if (dateType === 'from') {
+      return new Date(currentDate.setMonth(currentDate.getMonth() - DEFAULTS.DATE_RANGE));
+    } else if (dateType === 'to') {
+      return new Date(currentDate.setMonth(currentDate.getMonth() + DEFAULTS.DATE_RANGE));
+    }
+  }
 };
 
 // Function to handle pagination parameters
-const parsePaginationParams = (query) => {
-  const pageSize = parseInt(query.pageSize, 10) || DEFAULTS.PAGE_SIZE;
-  const page = parseInt(query.page, 10) || DEFAULTS.PAGE;
+const parsePaginationParams = (query, pageSize = null, page = 0) => {
+  const parsedPageSize = parseInt(query.pageSize, 10) || pageSize;
+  const parsedPage = parseInt(query.page, 10) || page;
   return {
-    pageSize: pageSize < 1 ? DEFAULTS.PAGE_SIZE : pageSize,
-    page: page < 0 ? DEFAULTS.PAGE : page,
+    pageSize: parsedPageSize < 1 ? pageSize : parsedPageSize,
+    page: parsedPage < 0 ? page : parsedPage,
   };
 };
 
 // Function to handle sort order
-const parseSortOrder = (sortOrder, next) => {
+const parseSortOrder = (sortOrder) => {
   if (sortOrder) {
     if (sortOrder.toLowerCase() === 'asc') return 1;
     if (sortOrder.toLowerCase() === 'desc') return -1;
-    next(new AppError('Invalid sortOrder parameter. Use "asc" or "desc".', 400));
+    // return new AppError('Invalid sortOrder parameter. Use "asc" or "desc".', 400);
   }
-  return DEFAULTS.SORT_ORDER;
+  return -1;
 };
 
 module.exports = { parseDate, parsePaginationParams, parseSortOrder };
