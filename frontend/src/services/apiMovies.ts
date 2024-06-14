1a85ea732c73d5f1eb056f5a6142f7e1bd522043
dsagi727 <dsagi727@gmail.com>
2024-06-14 15:10:19 +0200

    added getMoviesByQuery

--- frontend/src/services/apiMovies.ts
+++ frontend/src/services/apiMovies.ts
@@ -1,11 +1,40 @@
-import MovieModel from '../models/Movie.model';
+import MovieResponseModel from '../models/responseModel/MovieResponseModel';
 import { BASE_URL } from '../utils/constants';
 
-export const getMovies = async (): Promise<MovieModel[]> => {
+export const getMovies = async (): Promise<MovieResponseModel> => {
   const response = await fetch(`${BASE_URL}/movie`);
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
   const responseData = await response.json();
-  return responseData.data.movies;
+  return responseData;
 };
+
+export async function getMoviesByQuery(
+  genre: string,
+  sortOrder: string,
+  page: string,
+  pageSize: string,
+  fromDate?: string,
+  toDate?: string
+): Promise<MovieResponseModel> {
+  const params = new URLSearchParams();
+  params.append('genre', genre);
+  params.append('sortOrder', sortOrder);
+  params.append('page', page);
+  params.append('pageSize', pageSize);
+  if (fromDate) {
+    params.append('fromDate', fromDate);
+  }
+  if (toDate) {
+    params.append('toDate', toDate);
+  }
+
+  const response = await fetch(`${BASE_URL}/movie?${params.toString()}`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  const responseData = await response.json();
+  return responseData;
+}
