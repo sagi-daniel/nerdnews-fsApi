a5f9908a556612d733fc0c8e87997ee96d527688
dsagi727 <dsagi727@gmail.com>
2024-06-14 15:10:54 +0200

    changed layout style

--- frontend/src/components/parts/Main.tsx
+++ frontend/src/components/parts/Main.tsx
@@ -1,7 +1,9 @@
 import { ReactNode } from 'react';
 
 function Main({ children }: { children: ReactNode }) {
-  return <main className="flex flex-col mb-auto text-content-light dark:text-content-dark">{children}</main>;
+  return (
+    <main className="flex flex-col min-h-screen mb-auto text-content-light dark:text-content-dark">{children}</main>
+  );
 }
 
 export default Main;
