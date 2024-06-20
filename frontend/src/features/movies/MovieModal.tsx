dba5df8e3c84a5acabd23e2cccf32748f321c6da
dsagi727 <dsagi727@gmail.com>
2024-06-20 17:33:09 +0200

    added clickoutside hook  and changed style

--- frontend/src/features/movies/MovieModal.tsx
+++ frontend/src/features/movies/MovieModal.tsx
@@ -1,37 +1,29 @@
-import { FiX } from 'react-icons/fi';
+import { useOutsideClick } from '../../hooks/useOutsideClick';
 import { formatDateIsoToNormal } from '../../utils/helpers';
 import ImageLoader from '../../components/loaders/ImageLoader';
 import MovieModel from '../../models/Movie.model';
-import { useRef } from 'react';
+import CloseIcon from '../../components/CloseIcon';
 
 interface ModalProps {
   movie: MovieModel;
   closeModal: () => void;
 }
 
-function Modal({ movie, closeModal }: ModalProps) {
-  const modalRef = useRef<HTMLDivElement>(null);
-
-  const handleClickOutside = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
-      closeModal();
-    }
-  };
+function MovieModal({ movie, closeModal }: ModalProps) {
+  const modalRef = useOutsideClick({
+    handler: closeModal,
+    listenCapturing: true,
+  });
 
   return (
-    <div
-      className="z-20 fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex items-center justify-center"
-      onClick={handleClickOutside}
-    >
+    <div className="z-20 fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex items-center justify-center">
       <div
         ref={modalRef}
         className="relative flex flex-col md:flex-row gap-5 w-1/2 md:w-1/3 modal-content bg-bg-light dark:bg-bg-dark rounded-md shadow-md p-4"
       >
-        <button onClick={closeModal} className="absolute top-2 right-2 dark:text-primary">
-          <FiX className="text-xl" />
-        </button>
-        <div className="flex md:w-1/3">
-          <ImageLoader src={movie.poster} alt={movie.title} classes="rounded-md" />
+        <CloseIcon onClick={closeModal} />
+        <div className="flex h-full md:w-1/3">
+          <ImageLoader src={movie.poster} classes="rounded-md h-72" />
         </div>
         <div className="flex flex-col md:w-2/3">
           <h2 className="font-semibold mb-2">{movie.title}</h2>
@@ -43,4 +35,4 @@ function Modal({ movie, closeModal }: ModalProps) {
   );
 }
 
-export default Modal;
+export default MovieModal;
