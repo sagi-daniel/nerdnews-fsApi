a4948c53980a88b7b467a63488a5f704132d3856
dsagi727 <dsagi727@gmail.com>
2024-06-24 16:47:00 +0200

    added customToaster with utils color

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Toaster } from 'react-hot-toast';
+import CustomToaster from './components/CustomToaster';
 
 // GUARDS
 import ProtectedRoute from './features/auth/ProtectedRoute';
@@ -74,26 +74,7 @@ const App: React.FC = () => {
             <Route path="resetPassword/:resetToken" element={<ResetPasswordPage />} />
           </Routes>
         </BrowserRouter>
-        <Toaster
-          position="top-center"
-          gutter={12}
-          containerStyle={{ margin: '8px' }}
-          toastOptions={{
-            success: {
-              duration: 3000,
-            },
-            error: {
-              duration: 5000,
-            },
-            style: {
-              fontSize: '16px',
-              maxWidth: '500px',
-              padding: '16px 24px',
-              backgroundColor: 'var(--color-grey-0)',
-              color: 'var(--color-grey-700)',
-            },
-          }}
-        />
+        <CustomToaster />
       </DarkModeProvider>
     </QueryClientProvider>
   );
---
+++ frontend/src/components/CustomToaster.tsx
@@ -0,0 +1,28 @@
+import { Toaster } from 'react-hot-toast';
+
+import { FiCheckCircle, FiXCircle } from 'react-icons/fi';
+
+const SuccessIcon = () => <FiCheckCircle className="h-6 w-6 text-success-content" />;
+const ErrorIcon = () => <FiXCircle className="h-6 w-6 text-error-content" />;
+
+const CustomToaster = () => (
+  <Toaster
+    position="top-center"
+    gutter={12}
+    containerStyle={{ margin: '8px' }}
+    toastOptions={{
+      success: {
+        duration: 3000,
+        className: 'text-success-content bg-success text-sm max-w-[500px] p-4 flex items-center space-x-3',
+        icon: <SuccessIcon />,
+      },
+      error: {
+        duration: 5000,
+        className: 'text-error-content bg-error text-sm max-w-[500px] p-4 flex items-center space-x-3',
+        icon: <ErrorIcon />,
+      },
+    }}
+  />
+);
+
+export default CustomToaster;
