9d81d6dbcf67a144500b9897d1a2b10b0386f9df
dsagi727 <dsagi727@gmail.com>
2024-06-26 11:24:01 +0200

    add new styles

--- frontend/src/components/Sort.tsx
+++ frontend/src/components/Sort.tsx
@@ -8,16 +8,19 @@ interface SortProps {
 function Sort({ sortOrder, setSortOrder }: SortProps) {
   return (
     <div className="flex  md:flex-col  rounded-md">
-      <SelectField
-        options={[
-          { name: 'Újak elől', value: 'desc' },
-          { name: 'Régiek elől', value: 'asc' },
-        ]}
-        id="sortOrder"
-        label="Rendezés:"
-        value={sortOrder}
-        setValue={setSortOrder}
-      />
+      <div className="w-full md:w-full md:mb-3 ">
+        <SelectField
+          options={[
+            { name: 'Újak elől', value: 'desc' },
+            { name: 'Régiek elől', value: 'asc' },
+          ]}
+          id="sortOrder"
+          label="Rendezés:"
+          value={sortOrder}
+          setValue={setSortOrder}
+          required={false}
+        />
+      </div>
     </div>
   );
 }
