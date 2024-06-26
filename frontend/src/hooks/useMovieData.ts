409b1ba9c7703e1d3282a9093715467640f6b262
dsagi727 <dsagi727@gmail.com>
2024-06-26 16:56:13 +0200

    change query Model

--- frontend/src/hooks/useMovieData.ts
+++ frontend/src/hooks/useMovieData.ts
@@ -1,12 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import { getMoviesByQuery } from '../services/apiMovies';
-import MovieResponseModel from '../models/responseModel/MovieResponseModel';
 import useMoviesFilter from '../hooks/useMovieFilter';
+import MovieModel from '../models/Movie.model';
+import ResponseModel from '../models/Response.model';
 
 function useMoviesData() {
   const { genre, sortOrder, fromDate, toDate, page, pageSize } = useMoviesFilter().params;
 
-  return useQuery<MovieResponseModel, Error>(
+  return useQuery<ResponseModel<MovieModel[], 'movies'>, Error>(
     ['MoviesByQuery', genre, sortOrder, fromDate, toDate, page, pageSize],
     () => getMoviesByQuery(genre, sortOrder, fromDate, toDate, page, pageSize)
   );
