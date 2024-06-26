bb85eed115404b1d2cd72b1ce7e2a1c7ea8af201
dsagi727 <dsagi727@gmail.com>
2024-06-26 11:23:20 +0200

    add new resuable Modal component

---
+++ frontend/src/components/FormModal.tsx
@@ -0,0 +1,32 @@
+import { useOutsideClick } from '../hooks/useOutsideClick';
+import CloseIcon from './CloseIcon';
+
+interface FormModalProps {
+  title: string;
+  closeModal: () => void;
+  children: React.ReactNode;
+}
+
+function FormModal({ title, closeModal, children }: FormModalProps) {
+  const modalRef = useOutsideClick({
+    handler: closeModal,
+    listenCapturing: true,
+  });
+
+  return (
+    <div className="z-20 fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex items-center justify-center">
+      <div
+        ref={modalRef}
+        className="relative flex flex-col gap-5 w-1/2 md:w-1/3 modal-content bg-bg-light dark:bg-bg-dark rounded-md shadow-md p-4"
+      >
+        <CloseIcon onClick={closeModal} />
+        <div className="flex flex-col min-h-32 justify-between ">
+          <h2 className="text-center py-10">{title}</h2>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FormModal;
