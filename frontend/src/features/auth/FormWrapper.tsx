7c27a7ddb2699a97db66677be5b219441a4685fa
dsagi727 <dsagi727@gmail.com>
2024-06-26 11:25:26 +0200

    add default moveBack event

--- frontend/src/features/auth/FormWrapper.tsx
+++ frontend/src/features/auth/FormWrapper.tsx
@@ -1,19 +1,18 @@
 import { ReactNode } from 'react';
-import { useNavigate } from 'react-router-dom';
 import CloseIcon from '../../components/CloseIcon';
 import Logo from '../../components/Logo';
+import useMoveBack from '../../hooks/useMoveBack';
 
 type Size = 'small' | 'medium' | 'large';
 
 interface FormWrapperProps {
   label: string;
   size?: Size;
-  closePath?: string;
   children: ReactNode;
 }
 
-function FormWrapper({ children, label, closePath = '/home', size = 'small' }: FormWrapperProps) {
-  const navigate = useNavigate();
+function FormWrapper({ children, label, size = 'small' }: FormWrapperProps) {
+  const moveBack = useMoveBack();
 
   const sizeClasses = {
     small: 'w-full md:w-96',
@@ -25,7 +24,7 @@ function FormWrapper({ children, label, closePath = '/home', size = 'small' }: F
     <div className={`flex flex-col size-full items-center gap-16 justify-center`}>
       <Logo size="medium" />
       <div className={`relative ${sizeClasses[size]} bg-border-dark text-content-dark p-8 rounded-md w-110`}>
-        <CloseIcon onClick={() => navigate(closePath)} />
+        <CloseIcon onClick={moveBack} />
         <h2 className="mb-6 text-center">{label}</h2>
         {children}
       </div>
