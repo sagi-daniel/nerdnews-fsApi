1b89a54c22960ddc72518b3388330ce4157cebed
dsagi727 <dsagi727@gmail.com>
2024-06-27 11:00:06 +0200

    sources features implemented

---
+++ frontend/src/components/forms/SourceForm.tsx
@@ -0,0 +1,128 @@
+import { useState } from 'react';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { createSource, updateSource } from '../../services/apiSource';
+import { CategoryModel } from '../../models/Category.model';
+import { SourceModel } from '../../models/Source.model';
+import InputField from '../form-ui/InputField';
+import Button from '../Button';
+import toast from 'react-hot-toast';
+import SelectField from '../form-ui/SelectField';
+import { getCategories } from '../../services/apiCategory';
+import { capitalizeWord } from '../../utils/helpers';
+
+interface CategoryOption {
+  name: string;
+  value: string;
+}
+
+interface SourceFormProps {
+  source?: SourceModel | null;
+  setModalVisible: (value: boolean) => void;
+}
+
+function SourceForm({ source, setModalVisible }: SourceFormProps) {
+  const [sourceName, setSourceName] = useState(source?.sourceName || '');
+  const [sourceType, setSourceType] = useState(source?.sourceType || '');
+  const [sourceLink, setSourceLink] = useState(source?.sourceLink || '');
+  const [category, setCategory] = useState(source?.category._id || '');
+  const [comment, setComment] = useState(source?.comment || '');
+
+  const { data: categories } = useQuery(['CategoriesOptions'], getCategories);
+
+  const categoryNames = categories?.map((category: CategoryModel) => {
+    return { name: capitalizeWord(category.categoryName), value: category._id };
+  }) as CategoryOption[];
+
+  const queryClient = useQueryClient();
+
+  const { mutate: updateSourceMutate } = useMutation(updateSource, {
+    onSuccess: () => {
+      toast.success(`Forrás frissítve!`);
+      queryClient.invalidateQueries(['Sources']);
+    },
+  });
+
+  const { mutate: createSourceMutate } = useMutation(createSource, {
+    onSuccess: () => {
+      toast.success(`Forrás létrehozva!`);
+      queryClient.invalidateQueries(['Sources']);
+    },
+  });
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    const saveSource = {
+      sourceName,
+      sourceType,
+      sourceLink,
+      category: category,
+      comment,
+    };
+
+    if (source && source._id) {
+      const updatedSource = { ...source, ...saveSource };
+      const sourceId = source._id;
+      updateSourceMutate({ source: updatedSource, sourceId });
+      setModalVisible(false);
+    } else {
+      createSourceMutate(saveSource);
+      setModalVisible(false);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="grid grid-cols-2 gap-4">
+        <InputField
+          type="text"
+          id="sourceName"
+          label="Forrás neve"
+          value={sourceName}
+          setValue={setSourceName}
+          required={true}
+        />
+        <SelectField
+          options={[
+            { name: 'RSS', value: 'RSS' },
+            { name: 'RDF', value: 'RDF' },
+          ]}
+          id="sourceType"
+          label="Forrás típusa"
+          value={sourceType}
+          setValue={setSourceType}
+          required={true}
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <InputField
+          type="text"
+          id="sourceLink"
+          label="Forrás linkje"
+          value={sourceLink}
+          setValue={setSourceLink}
+          required={true}
+        />
+        <SelectField
+          id="category"
+          label="Kategória"
+          options={categoryNames}
+          value={category}
+          setValue={setCategory}
+          required={true}
+        />
+      </div>
+      <div className="mb-3">
+        <label className="block mb-1">Megjegyzés</label>
+        <textarea
+          id="comment"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          className="w-full rounded-md p-2 text-content-light font-semibold focus:outline-none focus:ring focus:ring-primary"
+        />
+      </div>
+      <Button type="submit" text={source ? 'Mentés' : 'Létrehozás'} size="full" />
+    </form>
+  );
+}
+
+export default SourceForm;
---
+++ frontend/src/pages/protectedPages/Sources.tsx
@@ -0,0 +1,111 @@
+import { useState } from 'react';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { SourceModel } from '../../models/Source.model';
+import { capitalizeWord, formatDateIsoToNormal } from '../../utils/helpers';
+import { deleteSource, getSources } from '../../services/apiSource';
+import Table, { Column } from '../../components/Table';
+import FormModal from '../../components/FormModal';
+import toast from 'react-hot-toast';
+import LoadingSpinner from '../../components/loaders/LoadingSpinner';
+import Error from '../../components/Error';
+import SourceForm from '../../components/forms/SourceForm';
+
+const sourceColumns: Column<SourceModel>[] = [
+  { key: 'createdAt', label: 'Létrehozva', formatter: formatDateIsoToNormal },
+  { key: 'sourceName', label: 'Kategória Neve' },
+  { key: 'sourceType', label: 'Típus' },
+  { key: 'sourceLink', label: 'Link' },
+  {
+    key: 'category',
+    label: 'Kategória',
+    formatter: (value) => capitalizeWord(value.categoryName),
+  },
+];
+
+function Sources() {
+  const [selectedSource, setSelectedSource] = useState<SourceModel | null>(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [confirmationVisible, setConfirmationVisible] = useState(false);
+  const [sourceIdToDelete, setSourceIdToDelete] = useState<string | null>(null);
+
+  const queryClient = useQueryClient();
+  const { data: sources, error, isLoading, isError } = useQuery(['Sources'], getSources);
+
+  const { mutate: deleteSourceMutate } = useMutation(deleteSource, {
+    onSuccess: () => {
+      toast.success(`Rss forrás törölve!`);
+      queryClient.invalidateQueries(['Sources']);
+    },
+  });
+
+  function handleCreate() {
+    setSelectedSource(null);
+    setModalVisible(true);
+  }
+
+  function handleUpdate(source: SourceModel) {
+    setSelectedSource(source);
+    setModalVisible(true);
+  }
+
+  function handleDelete(userId: string) {
+    setSourceIdToDelete(userId);
+    setConfirmationVisible(true);
+  }
+
+  function confirmDelete() {
+    if (sourceIdToDelete) {
+      deleteSourceMutate(sourceIdToDelete);
+      setConfirmationVisible(false);
+      setSourceIdToDelete(null);
+    }
+  }
+
+  const cancelDelete = () => {
+    setConfirmationVisible(false);
+    setSourceIdToDelete(null);
+  };
+
+  const closeModal = () => {
+    setSelectedSource(null);
+    setModalVisible(false);
+  };
+
+  if (isLoading) return <LoadingSpinner />;
+
+  if (isError) return <Error message={(error as Error).message} />;
+
+  return (
+    <>
+      {sources && (
+        <Table<SourceModel>
+          data={sources}
+          columns={sourceColumns}
+          onEdit={handleUpdate}
+          onDelete={handleDelete}
+          onCreate={handleCreate}
+        />
+      )}
+
+      {modalVisible && (
+        <FormModal title={selectedSource ? 'Szerkesztés' : 'Létrehozás'} closeModal={closeModal}>
+          <SourceForm source={selectedSource} setModalVisible={setModalVisible} />
+        </FormModal>
+      )}
+      {confirmationVisible && (
+        <FormModal title="Biztosan törölni szeretné az Rss forrást?" closeModal={cancelDelete}>
+          <div className="flex justify-end  space-x-2">
+            <button className="btn-delete" onClick={confirmDelete}>
+              Töröl
+            </button>
+            <button onClick={cancelDelete} className="btn-cancel">
+              Mégsem
+            </button>
+          </div>
+        </FormModal>
+      )}
+    </>
+  );
+}
+
+export default Sources;
