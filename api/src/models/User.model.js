926ffcbc476fb0d69470a060170960bf00dac6bc
dsagi727 <dsagi727@gmail.com>
2024-06-27 10:53:54 +0200

    remove spaces

--- api/src/models/User.model.js
+++ api/src/models/User.model.js
@@ -55,16 +55,13 @@ const UserSchema = mongoose.Schema(
 
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
-
   this.password = await bcrypt.hash(this.password, 12);
-
   this.passwordConfirm = undefined;
   next();
 });
 
 UserSchema.pre('save', function (next) {
   if (!this.isModified('password') || this.isNew) return next();
-
   this.passwordChangedAt = Date.now() - 1000;
   next();
 });
@@ -81,22 +78,15 @@ UserSchema.methods.correctPassword = async function (candidatePassword, userPass
 UserSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   if (this.passwordChangedAt) {
     const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
-
     return JWTTimestamp < changedTimestamp;
   }
-
   return false;
 };
 
 UserSchema.methods.createPasswordResetToken = function () {
   const resetToken = crypto.randomBytes(32).toString('hex');
-
   this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
-
-  console.log({ resetToken }, this.passwordResetToken);
-
   this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
-
   return resetToken;
 };
 
