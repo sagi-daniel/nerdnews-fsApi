d84258cf40f8f7c56fa56860f62b0a984c0ec759
dsagi727 <dsagi727@gmail.com>
2024-06-27 10:59:52 +0200

    categories features implemented

---
+++ frontend/src/components/forms/CategoryForm.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { CategoryModel, CreateCategoryModel, UpdateCategoryModel } from '../../models/Category.model';
+import { createCategory, updateCategory } from '../../services/apiCategory';
+import Button from '../Button';
+import toast from 'react-hot-toast';
+import InputField from '../form-ui/InputField';
+
+interface CategoryFormProps {
+  category?: CategoryModel | null;
+  setModalVisible: (value: boolean) => void;
+}
+
+function CategoryForm({ category, setModalVisible }: CategoryFormProps) {
+  const [categoryName, setCategoryName] = useState(category?.categoryName || '');
+
+  const queryClient = useQueryClient();
+
+  const { mutate: updateCategoryMutate } = useMutation(updateCategory, {
+    onSuccess: () => {
+      toast.success(`Kategória frissítve!`);
+      queryClient.invalidateQueries(['Categories']);
+    },
+  });
+
+  const { mutate: createCategoryMutate } = useMutation(createCategory, {
+    onSuccess: () => {
+      toast.success(`Kategória létrehozva!`);
+      queryClient.invalidateQueries(['Categories']);
+    },
+  });
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (category && category._id) {
+      const updateCategory: UpdateCategoryModel = {
+        categoryId: category._id,
+        category: categoryName,
+      };
+      console.log(updateCategory);
+      updateCategoryMutate(updateCategory);
+      setModalVisible(false);
+    } else {
+      const newCategory: CreateCategoryModel = {
+        categoryName,
+      };
+      console.log(newCategory);
+      createCategoryMutate(newCategory);
+      setModalVisible(false);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <InputField
+          type="text"
+          id="categoryName"
+          label="Kategória neve"
+          value={categoryName}
+          setValue={setCategoryName}
+          required={true}
+        />
+      </div>
+      <Button type="submit" text={category ? 'Mentés' : 'Létrehozás'} size="full" />
+    </form>
+  );
+}
+
+export default CategoryForm;
---
+++ frontend/src/pages/protectedPages/Categories.tsx
@@ -0,0 +1,101 @@
+import { useState } from 'react';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { CategoryModel } from '../../models/Category.model';
+
+import { deleteCategory, getCategories } from '../../services/apiCategory';
+import Table, { Column } from '../../components/Table';
+import FormModal from '../../components/FormModal';
+import CategoryForm from '../../components/forms/CategoryForm';
+import toast from 'react-hot-toast';
+import LoadingSpinner from '../../components/loaders/LoadingSpinner';
+import Error from '../../components/Error';
+
+const categoryColumns: Column<CategoryModel>[] = [{ key: 'categoryName', label: 'Kategória név' }];
+
+function Categories() {
+  const [selectedCategory, setSelectedCategory] = useState<CategoryModel | null>(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [confirmationVisible, setConfirmationVisible] = useState(false);
+  const [categoryIdToDelete, setCategoryIdToDelete] = useState<string | null>(null);
+
+  const queryClient = useQueryClient();
+  const { data: categories, error, isLoading, isError } = useQuery(['Categories'], getCategories);
+
+  const { mutate: deleteCategoryMutate } = useMutation(deleteCategory, {
+    onSuccess: () => {
+      toast.success(`Kategória törölve!`);
+      queryClient.invalidateQueries(['Categories']);
+    },
+  });
+
+  function handleCreate() {
+    setSelectedCategory(null);
+    setModalVisible(true);
+  }
+
+  function handleUpdate(category: CategoryModel) {
+    setSelectedCategory(category);
+    setModalVisible(true);
+  }
+
+  function handleDelete(categoryId: string) {
+    setCategoryIdToDelete(categoryId);
+    setConfirmationVisible(true);
+  }
+
+  function confirmDelete() {
+    if (categoryIdToDelete) {
+      deleteCategoryMutate(categoryIdToDelete);
+      setConfirmationVisible(false);
+      setCategoryIdToDelete(null);
+    }
+  }
+
+  const cancelDelete = () => {
+    setConfirmationVisible(false);
+    setCategoryIdToDelete(null);
+  };
+
+  const closeModal = () => {
+    setSelectedCategory(null);
+    setModalVisible(false);
+  };
+
+  if (isLoading) return <LoadingSpinner />;
+
+  if (isError) return <Error message={(error as Error).message} />;
+
+  return (
+    <>
+      {categories && (
+        <Table<CategoryModel>
+          data={categories}
+          columns={categoryColumns}
+          onEdit={handleUpdate}
+          onDelete={handleDelete}
+          onCreate={handleCreate}
+        />
+      )}
+
+      {modalVisible && (
+        <FormModal title={selectedCategory ? 'Szerkesztés' : 'Létrehozás'} closeModal={closeModal}>
+          <CategoryForm category={selectedCategory} setModalVisible={setModalVisible} />
+        </FormModal>
+      )}
+      {confirmationVisible && (
+        <FormModal title="Biztosan törölni szeretné a kategóriát?" closeModal={cancelDelete}>
+          <div className="flex justify-end  space-x-2">
+            <button className="btn-delete" onClick={confirmDelete}>
+              Töröl
+            </button>
+            <button onClick={cancelDelete} className="btn-cancel">
+              Mégsem
+            </button>
+          </div>
+        </FormModal>
+      )}
+    </>
+  );
+}
+
+export default Categories;
