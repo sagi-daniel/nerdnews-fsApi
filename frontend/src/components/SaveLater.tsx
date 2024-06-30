81e6aa7b4019ded812292f626079157ed7c3f5c2
dsagi727 <dsagi727@gmail.com>
2024-06-30 23:32:37 +0200

    implement saveLater function based on userContext

--- frontend/src/components/SaveLater.tsx
+++ frontend/src/components/SaveLater.tsx
@@ -1,50 +1,71 @@
-import { useEffect, useState } from 'react';
 import { FiStar } from 'react-icons/fi';
 import { GoStarFill } from 'react-icons/go';
-import { useUserContext } from '../context/UserContext';
-import { removeFromMyNews, addToMyNews } from '../services/apiMyNews';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import SaveLaterSkeleton from './loaders/skeletons/SaveLaterSkeleton';
+import { useAuth } from '../context/AuthContext';
+import { useUser } from '../context/UserContext';
+import { useEffect, useState } from 'react';
+import AuhtAlertModal from '../pages/authPages/AuhtAlertModal';
 
 interface SaveLaterProps {
-  newsId: string;
+  type: 'news' | 'movie';
+  itemId: string;
 }
 
-function SaveLater({ newsId }: SaveLaterProps) {
-  const { userNews, setUserNews } = useUserContext();
-  const [isSaved, setIsSaved] = useState(false);
+function SaveLater({ itemId, type }: SaveLaterProps) {
+  const { isAuthenticated } = useAuth();
 
-  const queryClient = useQueryClient();
+  const { news, movies, addToMyNews, removeFromMyNews, addToMyMovies, removeFromMyMovies } = useUser();
+  const [isSaved, setIsSaved] = useState<boolean | undefined>(false);
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
-    setIsSaved(userNews.some((news) => news._id === newsId));
-  }, [userNews, newsId]);
-
-  const { mutate: addSaveMutate } = useMutation(addToMyNews, {
-    onSuccess: (newNews) => {
-      setUserNews((prevNews) => [...prevNews, newNews]);
-      queryClient.invalidateQueries(['MyNews']);
-    },
-  });
-
-  const { mutate: removeSavedMutate } = useMutation(removeFromMyNews, {
-    onSuccess: (removedNewsId) => {
-      setUserNews((prevNews) => prevNews.filter((news) => news._id !== removedNewsId));
-      queryClient.invalidateQueries(['MyNews']);
-    },
-  });
+    if (type === 'news') {
+      setIsSaved(news?.some((item) => item._id === itemId));
+    } else if (type === 'movie') {
+      setIsSaved(movies?.some((item) => item._id === itemId));
+    }
+  }, [type, itemId, news, movies]);
 
   const handleSaveToggle = async () => {
     try {
-      if (isSaved) {
-        removeSavedMutate(newsId);
-      } else {
-        addSaveMutate(newsId);
+      if (type === 'news') {
+        if (isSaved) {
+          await removeFromMyNews(itemId);
+        } else {
+          await addToMyNews(itemId);
+        }
+      } else if (type === 'movie') {
+        if (isSaved) {
+          await removeFromMyMovies(itemId);
+        } else {
+          await addToMyMovies(itemId);
+        }
       }
+      setIsSaved((prev) => !prev); // Toggle the saved state
     } catch (err) {
-      console.error('Error updating user news:', err);
+      console.error('Error updating saved items:', err);
     }
   };
+
+  if (!isAuthenticated) {
+    return (
+      <>
+        <span
+          onClick={() => setModalVisible(!modalVisible)}
+          className="absolute m-1 p-2 rounded-full bg-bg-dark top-1 left-1 text-2xl z-[5] text-primary cursor-pointer"
+        >
+          <FiStar />
+        </span>
+        {modalVisible && (
+          <AuhtAlertModal
+            title="Kérjük, regisztrálj vagy jelentkezz be!"
+            description="A funkció használatához regisztráció szükséges. Kérjük, jelentkezz be, vagy hozd létre saját fiókodat az
+          alábbi gombok valamelyikére kattintva."
+            closeModal={() => setModalVisible(false)}
+          />
+        )}
+      </>
+    );
+  }
 
   return (
     <span
