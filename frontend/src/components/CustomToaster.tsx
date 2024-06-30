89f2e6b2b3a57119c11094bd739bc91f456b74a1
dsagi727 <dsagi727@gmail.com>
2024-06-30 23:28:42 +0200

    changed toaster style

--- frontend/src/components/CustomToaster.tsx
+++ frontend/src/components/CustomToaster.tsx
@@ -7,18 +7,18 @@ const ErrorIcon = () => <FiXCircle className="h-6 w-6 text-error-content" />;
 
 const CustomToaster = () => (
   <Toaster
-    position="top-center"
+    position="top-right"
     gutter={12}
     containerStyle={{ margin: '8px' }}
     toastOptions={{
       success: {
-        duration: 3000,
-        className: 'text-success-content bg-success text-lg max-w-[500px] p-4 flex items-center space-x-3',
+        duration: 5000,
+        className: 'text-primary-content bg-primary text-lg max-w-[500px] p-4 flex items-center space-x-2 rounded-md ',
         icon: <SuccessIcon />,
       },
       error: {
         duration: 5000,
-        className: 'text-error-content bg-error text-lg max-w-[500px] p-4 flex items-center space-x-3',
+        className: 'text-error-content bg-error text-lg max-w-[500px] p-2 flex items-center space-x-2 rounded-md',
         icon: <ErrorIcon />,
       },
     }}
