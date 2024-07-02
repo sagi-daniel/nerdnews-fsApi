a162f42b092d6a6ee2b71123f952056d0fd131f1
dsagi727 <dsagi727@gmail.com>
2024-07-02 10:11:52 +0200

    remove unnecessary type declarations

--- frontend/src/hooks/useNewsData.ts
+++ frontend/src/hooks/useNewsData.ts
@@ -1,15 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import { getNewsByQuery } from '../services/apiNews';
 import useNewsFilter from '../hooks/useNewsFilter';
-import ResponseModel from '../models/Response.model';
-import NewsModel from '../models/News.model';
 
 function useNewsData() {
   const { category, sortOrder, fromDate, toDate, page, pageSize } = useNewsFilter().params;
 
-  return useQuery<ResponseModel<NewsModel[], 'news'>, Error>(
-    ['NewsByQuery', category, sortOrder, fromDate, toDate, page, pageSize],
-    () => getNewsByQuery(category, sortOrder, fromDate, toDate, page, pageSize)
+  return useQuery(['NewsByQuery', category, sortOrder, fromDate, toDate, page, pageSize], () =>
+    getNewsByQuery(category, sortOrder, fromDate, toDate, page, pageSize)
   );
 }
 
