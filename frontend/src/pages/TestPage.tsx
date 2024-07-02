438e2ead9c919a67337d2ab93429b5182870e108
dsagi727 <dsagi727@gmail.com>
2024-07-02 10:14:10 +0200

    try buttons

--- frontend/src/pages/TestPage.tsx
+++ frontend/src/pages/TestPage.tsx
@@ -1,7 +1,14 @@
-import Users from './protectedPages/Users';
+import Button from '../components/Button';
+import Section from '../components/Section';
 
 function TestPage() {
-  return <Users />;
+  return (
+    <Section>
+      <Button type="submit" text="Button Primary" size="normal" style="primary" />
+      <Button type="button" text="Button Neutral" size="normal" style="neutral" />
+      <Button type="button" text="Button Cancel" size="normal" style="cancel" />
+    </Section>
+  );
 }
 
 export default TestPage;
