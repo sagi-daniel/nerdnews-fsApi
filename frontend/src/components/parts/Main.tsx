6c5411cb399aa3984e4d04c2ddc040759e9b7557
dsagi727 <dsagi727@gmail.com>
2024-07-02 10:03:02 +0200

    remove min-h-screen

--- frontend/src/components/parts/Main.tsx
+++ frontend/src/components/parts/Main.tsx
@@ -2,7 +2,9 @@ import { ReactNode } from 'react';
 
 function Main({ children }: { children: ReactNode }) {
   return (
-    <main className="flex flex-col min-h-screen mb-auto text-content-light dark:text-content-dark">{children}</main>
+    <main className="flex flex-col justify-center item-center text-content-light dark:text-content-dark">
+      {children}
+    </main>
   );
 }
 
