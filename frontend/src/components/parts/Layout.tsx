e1557e39968273f4d00a2d3e431e72fd4a9a8f28
dsagi727 <dsagi727@gmail.com>
2024-07-02 10:02:48 +0200

    refactoed style add h-screen

--- frontend/src/components/parts/Layout.tsx
+++ frontend/src/components/parts/Layout.tsx
@@ -1,7 +1,11 @@
 import { ReactNode } from 'react';
 
 function Layout({ children }: { children: ReactNode }) {
-  return <div className=" flex flex-col justify-between bg-bg-light dark:bg-bg-dark px-1 md:px-5 ">{children}</div>;
+  return (
+    <div className=" flex flex-col min-h-screen justify-between bg-bg-light dark:bg-bg-dark px-1 md:px-5 ">
+      {children}
+    </div>
+  );
 }
 
 export default Layout;
