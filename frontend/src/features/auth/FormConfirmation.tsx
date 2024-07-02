6ea584bbc6d2b84c8305b2306b0a8737036d5753
dsagi727 <dsagi727@gmail.com>
2024-07-02 10:06:39 +0200

    remove h.-screen class

--- frontend/src/features/auth/FormConfirmation.tsx
+++ frontend/src/features/auth/FormConfirmation.tsx
@@ -7,7 +7,7 @@ interface FormConfirmationProps {
 
 function FormConfirmation({ title, message }: FormConfirmationProps) {
   return (
-    <section className="relative h-screen text-content-light dark:text-content-dark">
+    <section className="relative text-content-light dark:text-content-dark">
       <div className={`flex flex-col size-full items-center  justify-center`}>
         <h1 className="text-3xl font-bold mb-4">{title}</h1>
         <p className="text-lg mb-4">{message}</p>
