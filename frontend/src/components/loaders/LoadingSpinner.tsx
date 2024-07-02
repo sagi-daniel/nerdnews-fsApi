c94fb895cef5045cd9429aaa7503f26dbf7ea370
dsagi727 <dsagi727@gmail.com>
2024-07-02 09:56:38 +0200

    new loadingspinner with farmer-motion

--- frontend/src/components/loaders/LoadingSpinner.tsx
+++ frontend/src/components/loaders/LoadingSpinner.tsx
@@ -1,12 +1,38 @@
+import { motion, Variants } from 'framer-motion';
+
+const variants: Variants = {
+  initial: {
+    scaleY: 0.5,
+    opacity: 0,
+  },
+  animate: {
+    scaleY: 1,
+    opacity: 1,
+    transition: {
+      repeat: Infinity,
+      repeatType: 'mirror',
+      duration: 1,
+      ease: 'circIn',
+    },
+  },
+};
+
 function LoadingSpinner() {
   return (
-    <div className="flex justify-center items-center size-full inset-0 overflow-hidden">
-      <div className="relative w-16 h-16 animate-spin">
-        <div className="absolute border-4 border-t-4 border-transparent border-t-primary rounded-full w-full h-full"></div>
-        <div className="absolute border-4 border-t-4 border-transparent  rounded-full w-12 h-12 top-2 left-2"></div>
-        <div className="absolute border-4 border-t-4 border-transparent  rounded-full w-8 h-8 top-4 left-4"></div>
-      </div>
-    </div>
+    <motion.div
+      transition={{
+        staggerChildren: 0.25,
+      }}
+      initial="initial"
+      animate="animate"
+      className="flex gap-1"
+    >
+      <motion.div variants={variants} className="h-12 w-2 bg-primary" />
+      <motion.div variants={variants} className="h-12 w-2 bg-primary" />
+      <motion.div variants={variants} className="h-12 w-2 bg-primary" />
+      <motion.div variants={variants} className="h-12 w-2 bg-primary" />
+      <motion.div variants={variants} className="h-12 w-2 bg-primary" />
+    </motion.div>
   );
 }
 
