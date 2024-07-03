25a7599b6ed85f1239186921ecca0db5263a95e0
dsagi727 <dsagi727@gmail.com>
2024-07-03 07:39:35 +0200

    changed style

--- frontend/src/components/parts/Footer.tsx
+++ frontend/src/components/parts/Footer.tsx
@@ -7,11 +7,15 @@ function Footer() {
   return (
     <footer>
       <Divider color="neutral" />
-      <div className=" mt-2 flex items-center pb-3 text-content-light dark:text-content-dark">
+      <div className=" mt-2 flex justify-center items-center pb-3 text-content-light dark:text-content-dark">
         <div className="w-full flex items-center justify-between">
           <Logo align="left" size="xsmall" />
           <CopyRight />
-          <CustomNavLink text="Adatkezelési tájékoztató" path="/privacy-policy" />
+          <CustomNavLink
+            text="Adatkezelési tájékoztató"
+            path="/privacy-policy"
+            className="text-xxs md:text-xs lg:text-sm text-center"
+          />
         </div>
       </div>
     </footer>
