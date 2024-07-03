c46052cfd793719910023792dd85ada2e8da1f17
dsagi727 <dsagi727@gmail.com>
2024-07-03 07:39:01 +0200

    changed style

--- frontend/src/components/ErrorPage.tsx
+++ frontend/src/components/ErrorPage.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
-import Logo from './Logo';
 import useMoveBack from '../hooks/useMoveBack';
+import Button from './Button';
 
 interface ErrorPageProps {
   errorCode: string | number;
@@ -14,22 +14,18 @@ function ErrorPage({ errorCode, errorMessage, errorDescription }: ErrorPageProps
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-content-light dark:text-content-dark">
-      <Logo align="center" size="small" />
+    <div className="flex flex-col items-center justify-center text-content-light dark:text-content-dark">
       <div className="text-center mt-10">
         <h1 className="text-9xl font-extrabold text-primary animate-pulse">{errorCode}</h1>
         <h3 className="text-2xl md:text-3xl mb-3">{errorMessage}</h3>
         <p className="text-lg md:text-xl mb-8">{errorDescription}</p>
         <div className="flex justify-center space-x-4">
-          <button onClick={moveBack} className="btn-cancel gap-1">
+          <Button style="cancel" onClick={moveBack}>
             <FiArrowLeft className="2xl" /> Vissza
-          </button>
-          <button
-            onClick={() => navigate('/')}
-            className="px-4 py-2 bg-primary text-primary-content rounded-md shadow-md hover:bg-primary-dark transition duration-300"
-          >
+          </Button>
+          <Button style="primary" onClick={() => navigate('/')}>
             Főoldal
-          </button>
+          </Button>
         </div>
       </div>
     </div>
