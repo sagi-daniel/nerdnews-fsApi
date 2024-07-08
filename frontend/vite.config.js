1f985706dac436a9cac6ecc2de15f114d856e1be
dsagi727 <dsagi727@gmail.com>
2024-07-08 17:54:12 +0200

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(), // React plugin
+    eslintPlugin({
+      // ESLint plugin with options
+      cache: false, // Disable ESLint cache
+      include: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'], // Specify files to lint
+    }),
+  ],
+  build: {
+    outDir: '../api/public', // Specify output directory for build
+    emptyOutDir: true, // Clear outDir before build
+  },
 });
