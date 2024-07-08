870355f97ec93751f75fd0f86f914f4030803bbe
dsagi727 <dsagi727@gmail.com>
2024-07-08 14:52:31 +0200

    removed focus animation

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -8,7 +8,6 @@ interface SearchBarProps {
 
 function SearchBar({ searchText, setSearchText }: SearchBarProps) {
   const [queryText, setQueryText] = useState<string>(searchText);
-  const [isFocused, setIsFocused] = useState<boolean>(false);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setQueryText(event.target.value);
@@ -17,14 +16,6 @@ function SearchBar({ searchText, setSearchText }: SearchBarProps) {
   const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setSearchText(queryText);
-  };
-
-  const handleFocus = () => {
-    setIsFocused(true);
-  };
-
-  const handleBlur = () => {
-    setIsFocused(false);
   };
 
   const handleClearInput = () => {
@@ -39,8 +30,6 @@ function SearchBar({ searchText, setSearchText }: SearchBarProps) {
         value={queryText}
         placeholder="Keresés"
         onChange={handleInputChange}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
         autoComplete="off"
         className={`transition-all duration-300 rounded-full py-[5px] px-4 bg-border-dark text-content-dark focus:outline-none focus:ring focus:ring-primary box-border pl-10 w-full' `}
       />
