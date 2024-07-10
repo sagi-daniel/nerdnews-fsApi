1acda716ff4e9707e60708c577fb78bdf70c0a44
dsagi727 <dsagi727@gmail.com>
2024-07-10 15:42:13 +0200

    removed poster

--- frontend/src/features/movies/MovieDetails.tsx
+++ frontend/src/features/movies/MovieDetails.tsx
@@ -1,33 +1,17 @@
 import { truncateText } from '../../utils/helpers';
 import { format } from 'date-fns';
 import MovieModel from '../../models/Movie.model';
-import useLoaderHook from '../../hooks/useLoaderHook';
-import SaveLater from '../../components/SaveLater';
 
 interface ModalProps {
   movie: MovieModel;
 }
 
 function MovieModal({ movie }: ModalProps) {
-  const loaded = useLoaderHook(movie.poster);
-
   return (
-    <div className="flex justify-center items-center py-5 size-full gap-4">
-      <div className="flex h-full md:w-1/3">
-        <div className={`relative size-full no-select z-1 overflow-hidden rounded-md`}>
-          <img
-            className={` select-none  rounded-md   ${loaded ? 'opacity-100' : 'opacity-0'}`}
-            src={movie.poster}
-            alt={movie.title}
-          />
-          <SaveLater itemId={movie._id} type="movie" />
-        </div>
-      </div>
-      <div className="flex flex-col md:w-2/3">
-        <h2 className="font-semibold mb-2">{movie.title}</h2>
-        <p>Megjelenés: {format(movie.release, 'yyyy.MM.dd.')}</p>
-        <p className="mt-2">{truncateText(movie.overview, 370)}</p>
-      </div>
+    <div className="flex flex-col justify-center  gap-2">
+      <h2 className="font-semibold mb-2">{movie.title}</h2>
+      <p>Megjelenés: {format(movie.release, 'yyyy.MM.dd.')}</p>
+      <p className="mt-2">{truncateText(movie.overview, 370)}</p>
     </div>
   );
 }
