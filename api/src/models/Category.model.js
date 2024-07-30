aeccdf0fa142d102d87386524554438b12e123c8
dsagi727 <dsagi727@gmail.com>
2024-07-30 09:13:17 +0200

    fix AppError path

--- api/src/models/Category.model.js
+++ api/src/models/Category.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const AppError = require('../../utils/appError');
+const AppError = require('../utils/appError');
 
 const CategorySchema = mongoose.Schema({
   categoryName: {
